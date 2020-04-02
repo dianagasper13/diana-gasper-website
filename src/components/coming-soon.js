d5289e4475b041cc876b7cc994486adcb9993442
dianasas <dianasas@icloud.com>
2020-04-01 20:50:10 -0400

    under construction svg

---
+++ src/components/coming-soon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComingSoonSVG from "../../assets/coming-soon-ai.svg"
+import "./coming-soon.css";
+
+const ComingSoon = () => {
+    return (
+        <div class="container">
+            <ComingSoonSVG></ComingSoonSVG>
+        </div>
+    )
+};
+export default ComingSoon;
